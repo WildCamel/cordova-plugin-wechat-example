e087269aa32fb3953ce9776b605e1c538e4d785f
WildCamel <lwg@LiWeigangdeMacBook-Pro.local>
2014-08-24 16:49:50 +0800

    Revert 9924fc4..1ca32f5
    This rolls back to commit 9924fc41e00386e6810538bfd1d53ef3fddb1cbb.

---
+++ platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,17 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/xu.li.cordova.wechat/www/wechat.js",
+        "id": "xu.li.cordova.wechat.Wechat",
+        "clobbers": [
+            "Wechat"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "xu.li.cordova.wechat": "0.3"
+}
+// BOTTOM OF METADATA
+});
